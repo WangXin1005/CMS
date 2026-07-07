f41a5bc4304b7272224b93105d13a27fee7ceb73
unknown <1005495524@qq.com>
2026-07-07 18:16:07 +0800

    fix: nginx 改用 command envsubst 替换 ${DOMAIN}，修复 entrypoint 脚本无执行权限

--- docker/nginx/docker-entrypoint.d/00-envsubst.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# 替换 nginx 配置中的 ${DOMAIN} 环境变量
-set -e
-ME=$(basename "$0")
-echo "$ME: 替换 ${DOMAIN} = ${DOMAIN:-localhost}"
-envsubst '${DOMAIN}' < /etc/nginx/conf.d/default.conf > /tmp/default.conf.tmp
-mv /tmp/default.conf.tmp /etc/nginx/conf.d/default.conf
